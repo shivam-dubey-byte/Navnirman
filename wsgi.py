a20f78a6adc9315eb132f5208fa197eeb26695b6
Shivam Raj Dubey <dubeyshivamraj@gmail.com>
2024-08-01 03:36:20 +0530

    2.3

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
-from app import app
+#from app import app
+from flask import Flask
 
+app=Flask(__name__)
+app.config['SECRET_KEY']='afasdfasdfadfasdfad#'
+
+@app.route("/")
+def index():
+    return "Hello World!"
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run()
