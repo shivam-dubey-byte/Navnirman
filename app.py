bdc4f54f34671a78d9d14ada8048cfc62b677a3a
Shivam Raj Dubey <74610989+shivam-dubey-byte@users.noreply.github.com>
2024-07-31 22:35:23 +0530

    1.6

--- app.py
+++ app.py
@@ -31,5 +31,4 @@ def donate():
 def donate1():
     return render_template("donate1.html",home='',about='',contact='',donate="active")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+
