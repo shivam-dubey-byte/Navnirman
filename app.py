6d2395f30d4a53413c3d2df6e737989f8bf80407
Shivam Raj Dubey <dubeyshivamraj@gmail.com>
2024-11-07 12:21:42 +0530

    07-11-2024 0.3

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('Home.html',home='active',about='',blog='',contact='',donate="",contact="")
-@app.route("/about")
-def about1():
     return render_template('AboutUs.html',home='',about='active',donate="",contact="")
+@app.route("/about")
+def history():
+    return render_template('Home.html',home='active',about='',donate="",contact="")
     #,home='active',about='',blog='',contact='',donate=""
 @app.route("/donate")
 def donate():
-    return render_template("donate.html",home='',about='',contact='',donate="active",contact="")
+    return render_template("donate.html",home='',about='',donate="active",contact="")
 
 
 if __name__ == '__main__':
